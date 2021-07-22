a32f57c10996bdb2d924d93933ec194da97f03ec
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2021-07-22 13:10:39 -0300

    Adicionando verificação de emulação/root no app antes da inicialização

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,31 @@ import React from 'react';
 import {AuthProvider} from './context/auth';
 import Routes from './routes';
 
+// validations
+import {useIsEmulator} from 'react-native-device-info';
+import JailMonkey from 'jail-monkey';
+import {StyleSheet, Text, View} from 'react-native';
+
 const App: React.FC = () => {
-  return (
+  const {result} = useIsEmulator();
+  const isAppTrusted = !JailMonkey.trustFall();
+
+  return isAppTrusted && !result ? (
     <AuthProvider>
       <Routes />
     </AuthProvider>
+  ) : (
+    <View style={style.container}>
+      <Text style={style.message}>
+        Emulador ou Celular com Root Ativo. (Substituir mensagem por componente)
+      </Text>
+    </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  message: {fontWeight: 'bold', fontSize: 15, padding: 15},
+});
 
 export default App;
